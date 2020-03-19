5952ee07818d8dad7bc7d8e6f634aefef5eccd2c
Aashir Hameed <aashirhameed@gmail.com>
2020-03-20 00:53:40 +0500

    Add project files.

---
+++ DealDadi/Migrations/202003131828346_first.cs
@@ -0,0 +1,20 @@
+namespace DealDadi.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class first : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "Name", c => c.String());
+            AddColumn("dbo.AspNetUsers", "ZipCode", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "ZipCode");
+            DropColumn("dbo.AspNetUsers", "Name");
+        }
+    }
+}
